f6456a9e993e527f570daf058b7ee2ff2ca72978
danielc766�@gmail.com <jppm1850@gmail.com>
2020-02-10 22:22:30 -0500

    cuarto commit

--- src/main/java/sesion01/Hola.java
+++ src/main/java/sesion01/Hola.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Hola {
 	public static void main(String[] args) {
 		//imprimir la entrada
-		System.out.println("bienvenido mitocyode 2020");
+		System.out.println("bienfvenido mitocyode 2020");
 		
 		//capturar valores de la consola
 		Scanner SC = new Scanner(System.in);
